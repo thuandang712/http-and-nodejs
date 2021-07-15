159c789236f9752db2681dedd0fd86cb8f54d248
Thuan Dang <thuandang@Thuans-Mac-mini.local>
2021-07-15 11:05:33 -0500

    code along learn content

---
+++ server.js
@@ -0,0 +1,82 @@
+// 'use strict';
+
+// var http = require('http');
+// var port = process.env.PORT || 8000;
+
+// var server = http.createServer(function(req, res) { // create instance of a server 
+//   var guests = ['Mary', 'Don'];
+//   var guestsJSON = JSON.stringify(guests); // turns array into a string to send via http
+
+//   res.setHeader('Content-Type', 'application/json');
+//   res.end(guestsJSON);
+// });
+
+// server.listen(port, function() { // tell server to listen on port 8000
+//   console.log('Listening on port', port);
+// });
+
+'use strict';
+
+var fs = require('fs');
+var path = require('path');
+var guestsPath = path.join(__dirname, 'guests.json');
+
+var http = require('http');
+var port = process.env.PORT || 8000;
+
+var server = http.createServer(function(req, res) {
+  if (req.method === 'GET' && req.url === '/guests') {
+    fs.readFile(guestsPath, 'utf8', function(err, guestsJSON) {
+      if (err) {
+        console.error(err.stack);
+        res.statusCode = 500;
+        res.setHeader('Content-Type', 'text/plain');
+        return res.end('Internal Server Error');
+      }
+
+      res.setHeader('Content-Type', 'application/json');
+      res.end(guestsJSON);
+    });
+  }
+  else if (req.method === 'GET' && req.url === '/guests/0') {
+    fs.readFile(guestsPath, 'utf8', function(err, guestsJSON) {
+      if (err) {
+        console.error(err.stack);
+        res.statusCode = 500;
+        res.setHeader('Content-Type', 'text/plain');
+        return res.end('Internal Server Error');
+      }
+
+      var guests = JSON.parse(guestsJSON);
+      var guestJSON = JSON.stringify(guests[0]);
+
+      res.setHeader('Content-Type', 'application/json');
+      res.end(guestJSON);
+    });
+  }
+  else if (req.method === 'GET' && req.url === '/guests/1') {
+    fs.readFile(guestsPath, 'utf8', function(err, guestsJSON) {
+      if (err) {
+        console.error(err.stack);
+        res.statusCode = 500;
+        res.setHeader('Content-Type', 'text/plain');
+        return res.end('Internal Server Error');
+      }
+
+      var guests = JSON.parse(guestsJSON);
+      var guestJSON = JSON.stringify(guests[1]);
+
+      res.setHeader('Content-Type', 'application/json');
+      res.end(guestJSON);
+    });
+  }
+  else {
+    res.statusCode = 404;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Not found');
+  }
+});
+
+server.listen(port, function() {
+  console.log('Listening on port', port);
+});
